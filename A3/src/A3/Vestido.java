b91010da6bd81840f849040c28a1259f01deb47d
Carlos Cesar <161955066+carlincc@users.noreply.github.com>
2024-06-06 18:02:04 -0300

    Delete A3/src/A3 directory

--- A3/src/A3/Vestido.java
+++
@@ -1,7 +0,0 @@
-public class Vestido extends Roupas {
-    public Vestido(String nome, double preco, String id, String cor, String tamanho, String tipo, String material, String estilo) {
-        super(nome , preco , id, cor , tamanho , tipo , material , estilo);
-    }
-
-   
-}
