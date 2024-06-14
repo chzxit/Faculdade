ecc86d26e9a9010eb9bb0fe85aebfbda9f67e7c4
Caio <njtrio7@gmail.com>
2024-06-14 19:07:02 -0200

    git push

---
+++ SISTEMADEGESTAODEROUPAS/SISTEMA/src/Sistema/Produto.java
@@ -0,0 +1,105 @@
+public  abstract class Produto {
+
+    private String nome;
+    private double preco;
+    private String id;
+    private String cor;
+    private String tamanho;
+    private String tipo;
+    private String material;
+    private String estilo;
+  
+
+    public Produto(String nome, double preco, String id, String cor, String tamanho, String tipo , String material, String estilo) {
+        this.nome = nome;
+        this.preco = preco;
+        this.id = id;
+        this.cor = cor;
+        this.tamanho = tamanho;
+        this.tipo = tipo;
+        this.material = material;
+        this.estilo = estilo; 
+        
+        
+    }
+   
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+
+    public String getMaterial(){
+        return material;
+    }
+
+    public void setMaterial(String material){
+        this.material = material;
+    }
+
+    public String getEstilo(){
+        return estilo;
+    }
+
+    public void setEstilo(String estilo){
+        this.estilo = estilo;
+    }
+
+    public void exibirInfo(){
+        
+    }
+
+    public abstract double CalcImpost();
+
+    @Override
+    public String toString(){ 
+        return  String.format(tipo);
+    }
+
+    
+ 
+
+}
